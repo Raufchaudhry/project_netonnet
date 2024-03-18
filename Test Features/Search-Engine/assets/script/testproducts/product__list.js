76d125863c973d70ffbc3e04d5e3b1a24fae09a9
Bigman578 <bwar62@gmail.com>
2024-03-18 09:48:17 +0100

    Products update

---
+++ Test Features/Search-Engine/assets/script/testproducts/product__list.js
@@ -0,0 +1,131 @@
+const netonnet__product = [
+    {
+        image: "./img/product/dator__surfplatta/laptop/1015638.webp",
+        image__alt: "1015638",
+        name: "MacBook Air. Nu med Apple M1-chippet.",
+        brand: "Apple",
+        description__text: "Apple MacBook Air 256GB M1 chip with 8-core CPU and 7-core GPU  Space Grey (MGN63KS/A).",
+        description__bulet: "Apple M1-chip med åttakärnig processor, 8 GB RAM + 256 GB SSD, 16-kärnig Neural Engine",
+        category__main: "Datorer och surfplator",
+        category__sub: "Laptop",
+        rating__stars: 5,
+        rating__ant: 90,
+        price: 11900,
+        price__sale: 9990,
+        tag: "Sänkt lagerpris"
+    },
+
+    {
+        image: "img/product/mobil__smartwatch/mobiltelefoner/1020710.webp",
+        image__alt: "1020710",
+        name: "iPhone 13 mini. Din nya superkraft.",
+        brand: "Apple",
+        description__text: "Apple iPhone 13 mini 128GB - Midnight",
+        description__bulet: "5,4-tums Super Retina XDR-skärm, Cinematiskt läge skiftar fokus automatiskt i dina videor, Avancerat dubbelkamerasystem",
+        category__main: "Mobil och smartwatch",
+        category__sub: "Mobil",
+        rating__stars: 4,
+        rating__ant: 68,
+        price: 8280,
+        price__sale: 7490,
+        tag: ""
+    },
+
+    {
+        image: "img/product/mobil__smartwatch/mobiltelefoner/1024953.webp",
+        image__alt: "1024953",
+        name: "OnePlus Nord CE 2 Lite med stort batteri och kraftfull prestanda.",
+        brand: "OnePlus",
+        description__text: "OnePlus Nord CE 2 Lite 6+128GB Black Dust",
+        description__bulet: "6,59' 120 Hz LCD-skärm, 6 GB RAM + 128 GB lagring, Snabbladdning & stort batteri",
+        category__main: "Mobil och smartwatch",
+        category__sub: "Mobil",
+        rating__stars: 3,
+        rating__ant: 2,
+        price: 2990,
+        price__sale: 2490,
+        tag: "Sänkt lagerpris"
+    },
+    
+    {
+        image: "img/product/tv/1029577.webp",
+        image__alt: "1029577",
+        name: "75' 4K UHD TV med levande färger",
+        brand: "Samsung",
+        description__text: "Samsung TU75CU7105KXXC",
+        description__bulet: "Uppskalar innehållet till 4K, Dynamisk kontrastförstärkare, Tizen OS Smart TV",
+        category__main: "TV",
+        category__sub: "75",
+        rating__stars: 4,
+        rating__ant: 13,
+        price: 15990,
+        price__sale: 9990,
+        tag: "Sänkt lagerpris"
+    },
+
+    {
+        image: "img/product/dator__surfplatta/laptop/1015648.webp",
+        image__alt: "1015648",
+        name: "MacBook Air. Nu med Apple M1-chippet.",
+        brand: "Apple",
+        description__text: "Apple MacBook Air 256GB Apple M1 chip with 8-core CPU and 7-core GPU Gold (MGND3KS/A)",
+        description__bulet: "Apple M1-chip med åttakärnig processor, 8 GB RAM + 256 GB SSD, 16-kärnig Neural Engine",
+        category__main: "Datorer och surfplator",
+        category__sub: "Laptop",
+        rating__stars: 5,
+        rating__ant: 90,
+        price: 119990,
+        price__sale: 9990,
+        tag: "Sänkt lagerpris"
+    },
+
+    {
+        image: "img/product/ljud/horlurar/1031322.webp",
+        image__alt: "1031322",
+        name: "Adaptivt ljud. Nu hör jag det.",
+        brand: "Apple",
+        description__text: "Apple AirPods Pro (2nd generation) with MagSafe (USB-C)",
+        description__bulet: "Aktiv brusreducering, Ladda via USB-C, Qi eller MagSafe, Tål svett och vatten",
+        category__main: "Ljud",
+        category__sub: "Hörlurar",
+        rating__stars: 5,
+        rating__ant: 8,
+        price: 3290,
+        price__sale: 2990,
+        tag: "Sänkt lagerpris"
+    },
+
+    {
+        image: "img/product/dator__surfplatta/laptop/1030082.webp",
+        image__alt: "1030082",
+        name: "14' laptop med 11:e Intel Core i5 & 12 h batteritid",
+        brand: "Lenovo",
+        description__text: "Lenovo IdeaPad 3 (82H7013CMX)",
+        description__bulet: "Intel® Core™ i5-1135G7 processor, 512 GB SSD & 16 GB RAM, Upp till 12 h batteritid",
+        category__main: "Datorer och surfplator",
+        category__sub: "Laptop",
+        rating__stars: 3,
+        rating__ant: 2,
+        price: 7990,
+        price__sale: 6490,
+        tag: "Sänkt lagerpris"
+    },
+
+    {
+        image: "img/product/tv/1023957.webp",
+        image__alt: "1023957",
+        name: "55' Smart 4K QLED-TV med Android 11 & HDR",
+        brand: "Andersson",
+        description__text: "Andersson QLED5531UHDA",
+        description__bulet: "4K Ultra HD-upplösning, Omslutande ljud med Dolby Atmos, Android™ 11 SmartTVd",
+        category__main: "TV",
+        category__sub: "55",
+        rating__stars: 4,
+        rating__ant: 152,
+        price: 6490,
+        price__sale: 4490,
+        tag: "Sänkt lagerpris"
+    }
+];
+
+window.netonnet__product = netonnet__product
