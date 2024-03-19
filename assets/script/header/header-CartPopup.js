e525de258b1d33e69d555f8cc53eadb674a297e1
Bigman578 <bwar62@gmail.com>
2024-03-19 09:02:27 +0100

    Functions added searchbar still pending, styling and setting up in progress

---
+++ assets/script/header/header-CartPopup.js
@@ -0,0 +1,23 @@
+let cartMenu = document.getElementById("cartMenu")
+let cartBtn = document.getElementById("cartBtn")
+
+function openCartMenu() {
+    cartMenu.style.display = "flex"
+}
+
+function closeCartMenu() {
+    cartMenu.style.display = "none"
+}
+
+cartBtn.addEventListener("click", openCartMenu);
+cartMenu.addEventListener("click", function(event) {
+    if (event.target === cartMenu) {
+        closeCartMenu()
+    } 
+});
+// window.addEventListener("resize", function() {
+//     if (window.innerWidth < 1200) {
+//         closeCartMenu()
+//     }
+// })
+
