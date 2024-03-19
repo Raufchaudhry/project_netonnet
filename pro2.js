1ce8ec67233949b0c9a892e7e8a0991ccb18c0ef
Bigman578 <bwar62@gmail.com>
2024-03-19 12:41:10 +0100

    P-A-I-N

---
+++ pro2.js
@@ -0,0 +1,23 @@
+
+function clickLeft() {
+  let container = document.querySelector('#card-container');
+  container.scrollTo({
+      left:container.scrollLeft - 200,
+      top: 0,
+      behavior: "smooth"
+  });
+}
+
+function clickRight() {
+  let container = document.querySelector('#card-container');
+  container.scrollTo({
+      left:container.scrollLeft + 200,
+      top: 0,
+      behavior: "smooth"
+  });
+}
+
+  
+
+
+    
