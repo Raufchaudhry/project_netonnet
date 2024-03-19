f3cb92b582af5c4da741b7e412446a5d7b2fa938
Bigman578 <bwar62@gmail.com>
2024-03-19 15:06:19 +0100

    rough editing of what is to come

--- assets/script/main/pro2.js
+++ assets/script/main/pro2.js
@@ -16,26 +16,3 @@ function clickRight() {
       behavior: "smooth"
   });
 }
-  
-  /*let card = 0;
-  function controller(x){
-     card = card + x;
-     slideshow(card);
-  }
-  slideshow(card);
-  function slideshow(num){
-    let slides = document.getElementsByClassName("slider");
-    if(num == slides.length){
-      card = 0;
-      num = 0;
-    }
-    if(num < 0){
-      card = slides.length-1;
-      num = slides.length-1;
-    }
-    for(let y of slides){
-  y.style.display = "none";
-    }
-    slides[num].style.display = "block";
-    }*/
-    
